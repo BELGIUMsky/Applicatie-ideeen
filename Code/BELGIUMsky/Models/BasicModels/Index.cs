dfd7033cf796eeb27f4f61dce9ee1289dcdea2de
BELGIUMsky <simon_poppe@hotmail.com>
2017-05-12 12:12:05 +0200

    domme test

---
+++ Code/BELGIUMsky/Models/BasicModels/Index.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BELGIUMsky.Models.BasicModels
+{
+    public class Index
+    {
+        public int Id { get; set; }
+
+        public Index() { }
+
+        public Index(int id)
+        {
+            this.Id = id;
+        }
+    }
+}
---
+++ Code/BELGIUMsky/Models/BasicModels/ItemVersion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BELGIUMsky.Models.BasicModels
+{
+    public class ItemVersion : Index
+    {
+        public DateTime CreationDate { get; set; }
+        public DateTime LastUpdatedDate { get; set; }
+
+        public ItemVersion() : base() { }
+
+        public ItemVersion(DateTime creationDate, DateTime lastUpdatedDate, int id) : base(id)
+        {
+            this.CreationDate = creationDate;
+            this.LastUpdatedDate = lastUpdatedDate;
+        }
+    }
+}
